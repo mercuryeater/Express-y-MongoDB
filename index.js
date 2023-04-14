10ba6d6ffd931998ccb9f7b62f1fe953b55ae65f
Federico <federicoc03@gmail.com>
2023-04-13 21:27:24 -0500

    Voy en el video 11, finalizadas la pp de toma de notas

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cookieSession = require("cookie-session");
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hola mundo!</h1>')
+
+app.set("view engine", "pug");
+app.set("views", "views");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  secret: "llave-secreta",
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+//MUESTRA LA LISTA DE NOTAS
+app.get("/", (req, res) => {
+  const notes = req.session.notes || [];
+  res.render("index", { notes });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+//MUESTRA EL FORMULARIO PARA CREAR UNA NOTA
+app.get("/notes/new", (req, res) => {
+  res.render("new");
+});
+
+//GUARDA-CREA UNA NOTA
+app.post("/notes", (req, res) => {
+  req.session.id = (req.session.id || 0) + 1;
+  let id = req.session.id;
+  req.session.notes = req.session.notes || [];
+  req.session.notes.push({ id: id, title: req.body.title, body: req.body.body});
+  res.redirect("/");
+
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo salio mal");
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
